0ce74798fa66726ad37dede08aca17755736a9af
anton-m-hung <84089123+anton-m-hung@users.noreply.github.com>
2024-09-23 15:32:36 -0400

    initial commit

---
+++ generate_data.R
@@ -0,0 +1,24 @@
+
+generate_data <- function(n) {
+  W1 <- runif(n, 0, 2)
+  W2_4 <- matrix(rbinom(n * 3, 1, 0.5), ncol = 3)
+  colnames(W2_4) <- paste0("W", 2:4)
+  W <- cbind(data.frame(W1 = W1), W2_4)
+  A <- g(W)
+  Y <- Q(cbind(W,A))
+  cbind(W, data.frame(A = A), data.frame(Y = Y))
+}
+
+g <- function(W) {
+  p <- with(W, plogis(W1 + W2*W3 - 2*W4))
+  rbinom(nrow(W), 1, p)
+}
+
+Q <- function(W_A) {
+  # expit(W1 + W2xW3 + W4xA - 3)
+  p <- with(W_A, plogis(W1 + W2*W3 + W4*A - 3))
+}
+
+test_data <- generate_data(100)
+
+head(test_data)
