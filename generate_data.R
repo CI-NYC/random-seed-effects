6c5af40b58533be01bd3c518d3ef2f5c48e70d89
anton-m-hung <84089123+anton-m-hung@users.noreply.github.com>
2024-09-26 14:50:29 -0400

    performance metrics

--- generate_data.R
+++ generate_data.R
@@ -25,45 +25,45 @@ Q <- function(W_A) {
   rbinom(nrow(W_A), 1, p)
 }
 
-test_100 <- generate_data(100)
+# test_100 <- generate_data(100)
 
-head(test_data)
+# head(test_data)
 
-tml3(
-  test_100,
-  trt = "A",
-  outcome = "Y",
-  covar = c("W1","W2","W3","W4"),
-  outcome_type="binomial",
-  folds=10,
-  learners_trt = c("glm","ranger","earth"),
-  learners_outcome = c("glm","ranger","earth")
-)
-
-
-test_500 <- generate_data(500)
-tml3(
-  test_500,
-  trt = "A",
-  outcome = "Y",
-  covar = c("W1","W2","W3","W4"),
-  outcome_type="binomial",
-  folds=10,
-  learners_trt = c("glm","ranger","earth"),
-  learners_outcome = c("glm","ranger","earth")
-)
-
-test_1000 <- generate_data(1000)
-res <- tml3(
-  test_1000,
-  trt = "A",
-  outcome = "Y",
-  covar = c("W1","W2","W3","W4"),
-  outcome_type="binomial",
-  folds=10,
-  learners_trt = c("glm","ranger","earth","lasso"),
-  learners_outcome = c("glm","ranger","earth","lasso")
-)
+# tml3(
+#   test_100,
+#   trt = "A",
+#   outcome = "Y",
+#   covar = c("W1","W2","W3","W4"),
+#   outcome_type="binomial",
+#   folds=10,
+#   learners_trt = c("glm","ranger","earth"),
+#   learners_outcome = c("glm","ranger","earth")
+# )
+# 
+# 
+# test_500 <- generate_data(500)
+# tml3(
+#   test_500,
+#   trt = "A",
+#   outcome = "Y",
+#   covar = c("W1","W2","W3","W4"),
+#   outcome_type="binomial",
+#   folds=10,
+#   learners_trt = c("glm","ranger","earth"),
+#   learners_outcome = c("glm","ranger","earth")
+# )
+# 
+# test_1000 <- generate_data(1000)
+# res <- tml3(
+#   test_1000,
+#   trt = "A",
+#   outcome = "Y",
+#   covar = c("W1","W2","W3","W4"),
+#   outcome_type="binomial",
+#   folds=10,
+#   learners_trt = c("glm","ranger","earth","lasso"),
+#   learners_outcome = c("glm","ranger","earth","lasso")
+# )
 
 # Distinction between "datasets" and "seeds"
 # 200 datasets (seeds 1 through 200)
@@ -82,34 +82,38 @@ run_analysis <- function(data){
   )
   
   # diff <- results$A1 - results$A0 Maybe don't need the difference?
-  cis <- c(rep(NA,6))
-  for (i in 1:3){
-    cis[(i*2-1):(i*2)] <- results$psis[i] + c(-1, 1)*qnorm(0.975)*results$ses[i]
-  }
+  # cis <- c(rep(NA,6))
+  # for (i in 1:3){
+  #   cis[(i*2-1):(i*2)] <- results$psis[i] + c(-1, 1)*qnorm(0.975)*results$ses[i]
+  # }
   
   # return vector of c(AA_estimate, AA_low, AA_high, A0_estimate, A0_low, A0_high, A1_estimate, A1_low, A1_high)
-  return(c(results$psis[1], cis[1:2], results$psis[2], cis[3:4], results$psis[3], cis[5:6]))
+  # return(c(results$psis[1], cis[1:2], results$psis[2], cis[3:4], results$psis[3], cis[5:6]))
   
+  return(c(results$psis, results$ses))
   
 }
 
+results_all <- list()
 # for (i in 1:200){
-# for (i in 1:5){
+for (i in 2){
   set.seed(i)
   data <- generate_data(100)
   
-  # results <- data.frame(matrix(ncol = 9, nrow = 150))
-  results <- data.frame(matrix(ncol = 9, nrow = 5))
+  results <- data.frame(matrix(ncol = 7, nrow = 150))
+  # results <- data.frame(matrix(ncol = 9, nrow = 5))
   tic()
-  # for (j in 1:150){
-  for (j in 1:5){
+  for (j in 1:150){
     set.seed(j)
-    results[j, ] <- run_analysis(data)
+    results[j, ] <- c(i,run_analysis(data))
   }
   toc()
+  names(results) <- c("dataset", "AA_estimate", "A0_estimate", "A1_estimate", "AA_se", "A0_se", "A1_se")
   
-# }
+  results_all[[i]]<-results
+}
 
 
-names(results) <- c("AA_estimate", "AA_low", "AA_high", "A0_estimate", "A0_low", "A0_high", "A1_estimate", "A1_low", "A1_high")
-
+# names(results) <- c("AA_estimate", "AA_low", "AA_high", "A0_estimate", "A0_low", "A0_high", "A1_estimate", "A1_low", "A1_high")
+results_all <- results_all[2:3]
+write.csv(results_all, "/Users/anton/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/intermediate-results/seeds_2_3.csv", row.names = F)
--- performance.R
+++ performance.R
@@ -1,23 +1,96 @@
-library(ggplot)
+library(ggplot2)
+library(dplyr)
 
+results <- read.csv("/Users/anton/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/intermediate-results/seed_1.csv") |>
+  mutate(dataset = 1) |>
+  select(dataset, AA_estimate, A0_estimate, A1_estimate, AA_se, A0_se, A1_se)
+
+results_2_3 <- readRDS("/Users/anton/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/intermediate-results/seeds_2_3.rds")
+
+######### 1
 # within-dataset variability of ATE point estimates and CI width
 # ATE = psi(1)- psi(0)
 # boxplots
 # confidence intervals? (how)
-ATE <- results$A1_estimate - results$A0_estimate
+results$ATE <- results$A1_estimate - results$A0_estimate
+results$CI_width <- 2*1.96*sqrt(results$A0_se^2 + results$A1_se^2)
+
+ggplot(results, aes(y=ATE)) +
+  geom_boxplot()
+
+ggplot(results, aes(y=CI_width)) +
+  geom_boxplot()
+
+# rbind the `results` dataframes for each of the seeds 1-200. create a "dataset" group column
+results_combined <- rbind(results, do.call(rbind, results_2_3))
+results_combined$ATE <- results_combined$A1_estimate - results_combined$A0_estimate
+results_combined$CI_width <- 2*1.96*sqrt(results_combined$A0_se^2 + results_combined$A1_se^2)
+
+# Calculate the mean of each group and rank them
+dataset_means <- results_combined |>
+  group_by(dataset) |>
+  summarise(mean_ATE = mean(ATE)) |>
+  arrange(mean_ATE) |>
+  mutate(rank = row_number())
+
+# Merge the rank back to the original dataframe
+results_combined <- results_combined |>
+  left_join(dataset_means, by = "dataset")
+
+ggplot(results_combined, aes(x=rank,y=ATE,group=rank,fill=rank)) +
+  geom_boxplot()
+
+ggplot(results_combined, aes(x=rank,y=CI_width,group=rank,fill=rank)) +
+  geom_boxplot()
 
 
-
-
+######## 2
 # Relative Within-Dataset Variability of ATE point estimates and CI widths
-# get the between-dataset variance using point estimates from one analysis
-# divide estimated within-dataset variance by the estimated between-datset variance
+# get the between-dataset variance using point estimates from analysis 1 (seed 1) of each dataset
+between_set_variance <- results_combined |>
+  group_by(dataset) |>
+  slice(1) |>
+  ungroup() |>
+  summarise(
+    estimate_variance = var(ATE),
+    CI_width_variance = var(CI_width)
+  )
 
+# Get within_set variance
+within_set_variance <- results_combined |>
+  group_by(dataset) |>
+  summarise(
+    estimate_variance = var(ATE),
+    CI_width_variance = var(CI_width)
+  )
 
+# divide estimated within-dataset variance by the estimated between-dataset variance
+relative_within_set_variance <- within_set_variance |>
+  mutate(estimate_variance = estimate_variance / between_set_variance$estimate_variance,
+         CI_width_variance = CI_width_variance / between_set_variance$CI_width_variance)
+
+######## 3 
 # Maximum, relative within-dataset range of CI bounds
 # range of upper bounds. range of lower bounds
+
+range_of_upper_bounds <- results_combined |>
+  # filter(dataset == 1) |>
+  group_by(dataset) |>
+  mutate(ATE_low = ATE - CI_width/2,
+         ATE_high = ATE + CI_width/2) |>
+  summarise(range_of_upper_bounds = max(ATE_high) - min(ATE_high),
+            mean_width = mean(CI_width))
+
+# mean_width <- results_combined |> 
+#   # filter(dataset == 1) |>
+#   group_by(dataset) |>
+#   summarise(mean_width = mean(CI_width))
+
+
 # all ranges - then find the mean of all ranges
 # divide range of upper bounds by mean of all ranges
+
+range_of_upper_bounds/mean_width
 
 
 # Unstable CIs (not clear on this)
@@ -26,5 +99,5 @@ ATE <- results$A1_estimate - results$A0_estimate
 # ^ unclear if means "pairs" of datasets, or just datasets that don't overlap with the entire group
 
 
-# Unstable Hypothesis testing (maybe not this one)
-# proportion over 150 seeds
+# Unstable Hypothesis testing
+# proportion over 150 seeds where null was accepted
