a78ed6f99f50e2fcf38b5af39e936ec4bbb84db8
anton-m-hung <84089123+anton-m-hung@users.noreply.github.com>
2024-11-26 14:47:16 -0500

    updated run.sh file

--- scripts/fold-simulation/run.sh
+++ scripts/fold-simulation/run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
+#SBATCH --account=msph
+#SBATCH --job-name=SchaderReplication
 #SBATCH --mem=5G
 #SBATCH --time=1:00:00
 #SBATCH --array=1-200
 
 cvfold=2
-
-module load R
 
 Rscript revised_sim_20230602.R $cvfold 
 
