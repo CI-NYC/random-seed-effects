953ab9b006ff66f633e7af064f042eaff95e339b
anton-m-hung <84089123+anton-m-hung@users.noreply.github.com>
2024-12-06 09:57:25 -0500

    finalized plots (initial confidence interval plot, boxplots, and histogram)

--- scripts/fold-simulation/combine_results.R
+++ scripts/fold-simulation/combine_results.R
@@ -1,12 +1,88 @@
 
+library(dplyr)
+library(ggplot2)
+library(gridExtra)
+library(scales)
+
 result_dir <- "~/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/scripts/fold-simulation/results/raw"
 
-results_2 <- do.call(rbind, lapply(1:200, function(i) {
-  result <- readRDS(file.path(result_dir, paste0("schader_40_", i, ".rds")))
-  estimate_se_list <- lapply(result, function(x) c(i, x$psis, x$ses))
+results_2 <- as.data.frame(do.call(rbind, lapply(1:200, function(i) {
+  result <- readRDS(file.path(result_dir, paste0("schader_2_", i, ".rds")))
+  estimate_se_list <- lapply(result, function(x) c(2, i, x$psis, x$ses))
   df <- do.call(rbind, estimate_se_list)
-}))
+})))
+
+results_40 <- as.data.frame(do.call(rbind, lapply(1:200, function(i) {
+  result <- readRDS(file.path(result_dir, paste0("schader_40_", i, ".rds")))
+  estimate_se_list <- lapply(result, function(x) c(40, i, x$psis, x$ses))
+  df <- do.call(rbind, estimate_se_list)
+})))
+
+results <- rbind(results_2, results_40)
+
+names(results) <- c("cv_folds","dataset","AA_estimate","A0_estimate","A1_estimate","AA_se","A0_se","A1_se")
+
+results <- results |>
+  mutate(ATE = A1_estimate - A0_estimate)
+
+dataset_means <- results |>
+  group_by(cv_folds, dataset) |>
+  summarise(mean_ATE = mean(ATE)) |>
+  arrange(mean_ATE) |>
+  mutate(rank = row_number())
+
+results <- results |>
+  left_join(dataset_means, by = c("cv_folds","dataset"))
+
+p2 <- ggplot(results |> filter(cv_folds == 2), aes(x=rank,y=ATE,group=rank)) +
+  geom_boxplot() +
+  ylim(-0.1, 0.35) +
+  ggtitle("Cross-fit folds: 2") +
+  xlab("Dataset") +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  theme_light()
+
+p40 <- ggplot(results |> filter(cv_folds == 40), aes(x=rank,y=ATE,group=rank)) +
+  geom_boxplot() +
+  ylim(-0.1, 0.35) +
+  ggtitle("Cross-fit folds: 40") +
+  xlab("Dataset") +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  theme_light()
+
+plot <- grid.arrange(p2, p40, ncol = 2)
+ggsave("~/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/scripts/fold-simulation/results/plots/boxplots_2v40.png", plot, width = 10, height = 5, units = "in", dpi = 300)
+
+results_variances <- results |>
+  group_by(cv_folds,dataset) |>
+  summarise(dataset_variance = var(ATE))
+
+p2 <- ggplot(results_variances |> filter(cv_folds == 2), aes(x=dataset_variance)) +
+  geom_histogram(bins=50, fill = "grey40") +
+  ggtitle("Cross-fit folds: 2") +
+  xlab("Variance") +
+  scale_x_continuous(labels = scales::label_scientific(scale = 1e0),
+                     limits = c(NA,0.001)) +
+  theme_light()
+
+p40 <- ggplot(results_variances |> filter(cv_folds == 40), aes(x=dataset_variance)) +
+  geom_histogram(bins=50, fill = "grey40") +
+  ggtitle("Cross-fit folds: 40") +
+  xlab("Variance") +
+  scale_x_continuous(labels = scales::label_scientific(scale = 1e0),
+                     limits = c(NA,0.001)) +
+  theme_light()
+
+plot <- grid.arrange(p2, p40, ncol=2)
+ggsave("~/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/scripts/fold-simulation/results/plots/variances_2v40.png", plot, width = 10, height = 5, units = "in", dpi = 300)
 
 
+# Variance calculations
+var1 <- var(results_variances |> filter(cv_folds==2) |> pull(dataset_variance))
+var2 <- var(results_variances |> filter(cv_folds==40) |> pull(dataset_variance))
 
+var1/var2
 
+results_variances_medians <- results_variances |>
+  group_by(cv_folds) |>
+  summarise(med = median(dataset_variance))
--- scripts/replicate-figure-1/generate_data_fig_1/performance_seeds_12_45.R
+++ scripts/replicate-figure-1/generate_data_fig_1/performance_seeds_12_45.R
@@ -4,7 +4,7 @@ library(gridExtra)
 
 # 2 folds
 
-results_2 <- rbind(readRDS("/Users/anton/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/fig_1_results/revised_seeds_1-50.rds"))
+results_2 <- rbind(readRDS("~/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/paper/fig_1_results/revised_seeds_1-50.rds"))
 
 results_2 <- results_2 |> 
   mutate(ATE = A1_estimate - A0_estimate,
@@ -20,14 +20,16 @@ results_2_45 <- results_2 |>
 p_2_45 <- ggplot(results_2_45, aes(x = rank, y = ATE)) +
   geom_errorbar(aes(ymin = ATE_low, ymax = ATE_high, color=ATE_high<0 | ATE_low>0)) +
   geom_point() + 
+  xlab("Run") +
   ylim(-0.2, 0.4) +
   ggtitle("Cross-fit folds: 2") +
   geom_hline(yintercept = 0, linetype = "dashed") +
+  theme_light() +
   theme(legend.position = "none")
 
 
 
-results_40 <- rbind(readRDS("/Users/anton/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/fig_1_results/seeds_45_40folds.rds"))
+results_40 <- rbind(readRDS("~/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/paper/fig_1_results/seeds_45_40folds.rds"))
 
 results_40 <- results_40 |> 
   mutate(ATE = A1_estimate - A0_estimate,
@@ -43,9 +45,11 @@ results_40_45 <- results_40 |>
 p_40_45 <- ggplot(results_40_45, aes(x = rank, y = ATE)) +
   geom_errorbar(aes(ymin = ATE_low, ymax = ATE_high), color="#00BFC4") +
   geom_point() + 
+  xlab("Run") +
   ylim(-0.2, 0.4) +
   ggtitle("Cross-fit folds: 40") +
   geom_hline(yintercept = 0, linetype = "dashed") +
+  theme_light() +
   theme(legend.position = "none")
 
 # results_40_12 <- results_40 |>
@@ -62,7 +66,7 @@ p_40_45 <- ggplot(results_40_45, aes(x = rank, y = ATE)) +
 #   theme(legend.position = "none")
 
 
-results_20 <- rbind(readRDS("/Users/anton/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/fig_1_results/seeds_12_45.rds"))
+results_20 <- rbind(readRDS("~/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/paper/fig_1_results/seeds_12_45.rds"))
 
 results_20 <- results_20 |> 
   mutate(ATE = A1_estimate - A0_estimate,
@@ -94,11 +98,13 @@ p_20_45 <- ggplot(results_20_45, aes(x = rank, y = ATE)) +
   ylim(-0.2, 0.4) +
   ggtitle("Cross-fit folds: 20") +
   geom_hline(yintercept = 0, linetype = "dashed") +
+  xlab("Run") +
+  theme_light() +
   theme(legend.position = "none")
 
 plot <- grid.arrange(p_2_45, p_20_45, p_40_45, ncol = 3, top = ("Dataset seed 45"))
-ggsave("/Users/anton/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/fig_1_results/folds_2_20_40.pdf", plot, width = 9, height = 4, units = "in", dpi = 300)
+ggsave("~/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/scripts/fold-simulation/results/plots/folds_2_20_40.png", plot, width = 9, height = 4, units = "in", dpi = 300)
 
-plot <- grid.arrange(p_20_45, p_40_45, ncol = 2, top = ("Dataset seed 45"))
-ggsave("/Users/anton/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/fig_1_results/folds_20_40.pdf", plot, width = 6, height = 4, units = "in", dpi = 300)
+plot <- grid.arrange(p_2_45, p_40_45, ncol = 2)
+ggsave("~/Library/CloudStorage/OneDrive-ColumbiaUniversityIrvingMedicalCenter/Q3_2024/random-seed-effects/scripts/fold-simulation/results/plots/folds_2_40.png", plot, width = 10, height = 5, units = "in", dpi = 300)
 
